56c612d0d423f3abdb13690398eb43de04a783af
Cleyton Scherer <cleyton.scherer@schwancosmetics.com>
2022-08-06 12:03:27 -0300

    Commit Inicial #1

---
+++ src/main/java/com/schwancosmetics/c4/seniorerp/SeniorerpApplication.java
@@ -0,0 +1,13 @@
+package com.schwancosmetics.c4.seniorerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorerpApplication.class, args);
+	}
+
+}
